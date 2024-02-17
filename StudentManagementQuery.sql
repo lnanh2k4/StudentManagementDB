92dd297aad309c31ea854c4af4a993161e342b2a
lnanh2k4 <lnanh2k4@gmail.com>
2024-02-17 12:10:36 +0700

    edit correct query in sql

--- StudentManagementQuery.sql
+++ StudentManagementQuery.sql
@@ -50,40 +50,40 @@ CREATE TABLE Curriculum(
 CuID NVARCHAR(11),
 CuName NVARCHAR(100),
 CuDes TEXT,
+SpID NVARCHAR(3),
 CONSTRAINT PK_Curriculum PRIMARY KEY (CuID)
+CONSTRAINT FK_CurriculumSpecialization FOREIGN KEY (SpID) REFERENCES Specialization(SpID),
 )
 GO
 
-INSERT INTO Curriculum(CuID,CuName,CuDes) VALUES ('','',''),
-												 ('','','')
+INSERT INTO Curriculum(CuID,CuName,CuDes,SpID) VALUES ('','','',''),
+													 ('','','','')
 GO
 
 CREATE TABLE Specialization(
 SpID NVARCHAR(3),
 SpName NVARCHAR(30),
 MaID NVARCHAR(3),
-CuID NVARCHAR(11),
 CONSTRAINT PK_Specialization PRIMARY KEY (SpID),
 CONSTRAINT FK_Specialization FOREIGN KEY (MaID) REFERENCES Major(MaID),
-CONSTRAINT FK_Specialization_Curriculum FOREIGN KEY (CuID) REFERENCES Curriculum(CuID)
 )
 GO
 
-INSERT INTO Specialization(SpID,SpName,MaID,CuID) VALUES ('FIN', 'Finance','BA',''),
-													('HM', 'Hotel management','BA',''),
-													('IB', 'International Business','BA',''),
-													('MC', 'Multimedia Communication','BA',''),
-													('MKT', 'Digital Marketing','BA',''),
-													('TM', 'Tourism and Travel Management','BA',''),
+INSERT INTO Specialization(SpID,SpName,MaID) VALUES ('FIN', 'Finance','BA'),
+													('HM', 'Hotel management','BA'),
+													('IB', 'International Business','BA'),
+													('MC', 'Multimedia Communication','BA'),
+													('MKT', 'Digital Marketing','BA'),
+													('TM', 'Tourism and Travel Management','BA'),
 													('EN', 'English Studies','ES',''),
-													('AI', 'Artificial Intelligence','IT',''),
-													('GD', 'Digital Art & Design','IT',''),
-													('IA', 'Information Assurance','IT',''),
-													('IoT','Internet of Things','IT',''),
-													('IS', 'Information System','IT',''),
-													('SE', 'Software Engineering','IT',''),
-													('JP', 'Japanese Studies','JP',''),
-													('KR', 'Korean Studies','KR','')
+													('AI', 'Artificial Intelligence','IT'),
+													('GD', 'Digital Art & Design','IT'),
+													('IA', 'Information Assurance','IT'),
+													('IoT','Internet of Things','IT'),
+													('IS', 'Information System','IT'),
+													('SE', 'Software Engineering','IT'),
+													('JP', 'Japanese Studies','JP'),
+													('KR', 'Korean Studies','KR')
 GO
 
 
