cca1eee341eb25c01c217fd53ac88f615ace2f32
HoangTyy <mrmastervp123@gmail.com>
2024-02-18 15:04:53 +0700

    update comment

--- StudentManagementQuery.sql
+++ StudentManagementQuery.sql
@@ -64,7 +64,7 @@ CuID NVARCHAR(11), -- Declare the ID of the training program as a String that ca
 CuName NVARCHAR(100), -- Declare the Name of the training program as a String that can be written in Vietnamese
 SpID NVARCHAR(3), -- Declare the ID of the major as a String that can be written in Vietnamese
 CONSTRAINT PK_Curriculum PRIMARY KEY (CuID), -- Add primary key condition PK_Curriculum by CuID
-CONSTRAINT FK_CurriculumSpecialization FOREIGN KEY (SpID) REFERENCES Specialization(SpID), -- Add primary key condition FK_CurriculumSpecialization by SpID
+CONSTRAINT FK_CurriculumSpecialization FOREIGN KEY (SpID) REFERENCES Specialization(SpID), -- Add foreign key condition FK_CurriculumSpecialization by SpID
 )
 GO
 -- Add data for the curriculum table
