57b37ac6fe81357070d289bb12122b35b15f4353
HoangTyy <mrmastervp123@gmail.com>
2024-03-08 15:35:00 +0700

    update campus

--- Script/Campus/CREATE/Create Campus.sql
+++ Script/Campus/CREATE/Create Campus.sql
@@ -3,7 +3,7 @@ CaID NVARCHAR(8), -- The Campus ID is declared as a String type that can be writ
 CaName NVARCHAR(30), -- The Campus Name is declared as a String type that can be written in Vietnamese
 CaAddress NVARCHAR(255), -- The Campus Address is declared as a String type that can be written in Vietnamese
 CaEmail NVARCHAR(30), -- The Email of Campus is declared as a String type that can be written in Vietnamese
-CaHotline NVARCHAR(15), -- The Hotline of Campus is declared as a String type that can be written in Vietnamese
+CaHotline NVARCHAR(30), -- The Hotline of Campus is declared as a String type that can be written in Vietnamese
 CONSTRAINT PK_Campus PRIMARY KEY (CaID) -- Add primary key condition PK_Campus by CaID
 )
 GO
