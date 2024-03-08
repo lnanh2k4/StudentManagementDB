ed5699da5d63e43d010b77adac4bfe1e56997098
Le Ngoc Phong Nha <phongnhale197@gmail.com>
2024-03-08 08:17:00 -0800

    updated content

--- StudentManagementQuery.sql
+++ StudentManagementQuery.sql
@@ -552,9 +552,9 @@ INSERT INTO Curriculum(CuID,CuName,SpID) VALUES
 ('BKR_KR_18C','Bachelor Program of Korean Studies, Korean Studies for level 2-3 of the Preparatory English period','KR'),
 ('BKR_KR_18D','Bachelor Program of Korean Studies, Korean Studies for level 1 of the Preparatory English period','KR'),
 
-('BJP_JP_18D','Bachelor Program of Japanese Studies, Japanese Studies for exemption from preparatory English lessons','JP'),
-('BJP_JP_18D','Bachelor Program of Japanese Studies, Japanese Studies for level 4-6 of the Preparatory English period','JP'),
-('BJP_JP_18D','Bachelor Program of Japanese Studies, Japanese Studies for level 2-3 of the Preparatory English period','JP'),
+('BJP_JP_18A','Bachelor Program of Japanese Studies, Japanese Studies for exemption from preparatory English lessons','JP'),
+('BJP_JP_18B','Bachelor Program of Japanese Studies, Japanese Studies for level 4-6 of the Preparatory English period','JP'),
+('BJP_JP_18C','Bachelor Program of Japanese Studies, Japanese Studies for level 2-3 of the Preparatory English period','JP'),
 ('BJP_JP_18D','Bachelor Program of Japanese Studies, Japanese Studies for level 1 of the Preparatory English period','JP')
 GO
 
